4c1c3bb8fc935652b2425ee339b361a9698af79a
Mustakim Shaikh <107025272+MUSTAKIMSHAIKH2942@users.noreply.github.com>
2024-10-19 14:59:17 +0530

    Add files via upload

---
+++ Remarks changes/sampledalclass/RemarksDetails.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class RemarksDetails
+    ' Private fields for encapsulation
+    Private _SampleID As String
+    Private _UserName As String
+    Private _Remarks As String
+
+    ' Properties to expose private fields
+    Public Property SampleID() As String
+        Get
+            Return _SampleID
+        End Get
+        Set(ByVal value As String)
+            _SampleID = value
+        End Set
+    End Property
+
+    Public Property UserName() As String
+        Get
+            Return _UserName
+        End Get
+        Set(ByVal value As String)
+            _UserName = value
+        End Set
+    End Property
+
+    Public Property Remarks() As String
+        Get
+            Return _Remarks
+        End Get
+        Set(ByVal value As String)
+            _Remarks = value
+        End Set
+    End Property
+
+    ' Method to insert data into Remarks table using the stored procedure
+    Public Shared Function InsertRemarksDetails(ByVal remarksDetails As RemarksDetails) As Boolean
+        Try
+            ' Prepare SQL parameters
+            Dim params(2) As SqlParameter
+            params(0) = New SqlParameter("@SampleReqizitionID", remarksDetails.SampleID)
+            params(1) = New SqlParameter("@UserName", remarksDetails.UserName)
+            params(2) = New SqlParameter("@Remarks", remarksDetails.Remarks)
+
+            ' Specify the name of the stored procedure
+            Dim storedProcName As String = "InsertRemarks"
+
+            ' Execute the stored procedure and return true if successful
+            Return SqlHelper.ExecuteNonQuery(WebGlobalVariables.Connection2, CommandType.StoredProcedure, storedProcName, params) > 0
+        Catch ex As Exception
+            ' Log or handle exception
+            Return False
+        End Try
+    End Function
+End Class
---
+++ Remarks changes/samplefolderchanges/remarks.aspx.vb
@@ -0,0 +1,45 @@
+﻿Imports System
+Imports System.Data
+Imports System.Data.SqlClient
+
+Partial Class SampleRequisition_remarks
+    Inherits System.Web.UI.Page
+
+    Private Shared lockObject As New Object() ' Locking object for thread safety
+
+    Protected Sub btnSubmit_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnSubmit.Click
+        ' Use a lock to ensure only one thread can execute this block at a time
+        SyncLock lockObject
+            Dim username As String = Request.QueryString("Name")
+            Dim SampleReqizitionID As Integer
+            Dim remarks As String = txtRemarks.Text.Trim()
+
+            If Integer.TryParse(Request.QueryString("id"), SampleReqizitionID) Then
+                If Not String.IsNullOrEmpty(remarks) Then
+                    Dim remarksDetails As New RemarksDetails()
+                    remarksDetails.SampleID = SampleReqizitionID.ToString()
+                    remarksDetails.UserName = username
+                    remarksDetails.Remarks = remarks
+
+                    ' Check for existing remark before inserting
+                    remarksDetails.InsertRemarksDetails(remarksDetails)
+
+                     ClearFormFields()
+                    Response.Redirect("ViewSampleRequisition.aspx")
+                Else
+                    lblMessage.Text = "Remarks cannot be empty."
+                End If
+            Else
+                lblMessage.Text = "Invalid Sample Request ID."
+            End If
+        End SyncLock
+    End Sub
+
+    Private Sub ClearFormFields()
+        txtRemarks.Text = String.Empty
+    End Sub
+
+    Protected Sub btnCancel_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnBack.Click
+        Response.Redirect("ViewSampleRequisition.aspx")
+    End Sub
+End Class
